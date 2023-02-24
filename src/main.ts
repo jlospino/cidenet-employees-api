4acc2193a52016bcc16ec31da8f65883bcc44bde
Jose Ospino <ospinojose36@gmail.com>
2023-02-24 00:35:08 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors();
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
